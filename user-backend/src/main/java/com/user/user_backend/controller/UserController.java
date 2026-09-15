ac56108af183ec1229693623f3c45ea9e2a493b7
noxolo <sibisinoxolosthobile@gmail.com>
2026-09-15 19:15:24 +0200

    Adding CORS

--- user-backend/src/main/java/com/user/user_backend/controller/UserController.java
+++ user-backend/src/main/java/com/user/user_backend/controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/users")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private final UserRepository userRepository;
