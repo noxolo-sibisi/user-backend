4cc15b2bcee43e173778f286f62f86a727d89061
noxolo <sibisinoxolosthobile@gmail.com>
2026-09-18 10:10:31 +0200

    added global configuration

--- user-backend/src/main/java/com/user/user_backend/controller/UserController.java
+++ user-backend/src/main/java/com/user/user_backend/controller/UserController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/users")
-@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private final UserRepository userRepository;
