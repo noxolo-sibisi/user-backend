c3e002310c5a9b400dc0799f332ca3ff1c61f10f
noxolo <sibisinoxolosthobile@gmail.com>
2026-09-19 18:39:53 +0200

    added global configuration

--- user-backend/src/main/java/com/user/user_backend/CorsConfig.java
+++ user-backend/src/main/java/com/user/user_backend/CorsConfig.java
@@ -5,10 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig  implements WebMvcConfigurer {
-
+public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
